4f68ddf02b9167efd37e423b7ffa90a749c06437
Ulthwe <allodmefist@mail.ru>
2021-01-10 16:16:08 +0300

    test: context load test

---
+++ src/test/java/com/github/maxkazantsev/rabbitmq/RabbitConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maxkazantsev.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
