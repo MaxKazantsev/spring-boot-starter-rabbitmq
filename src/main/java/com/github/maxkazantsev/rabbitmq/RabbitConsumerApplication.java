7aa36a9c8089a056467c63ee55e994e257757917
Ulthwe <allodmefist@mail.ru>
2021-01-10 16:10:56 +0300

    feat: application initial config

---
+++ src/main/java/com/github/maxkazantsev/rabbitmq/RabbitConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.maxkazantsev.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitConsumerApplication.class, args);
+    }
+
+}
